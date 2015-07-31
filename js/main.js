f22e51b5e2fdb5fdd1b9cf91cedcc5bfd4961eb9
ishashakti <krshna108@gmail.com>
2015-07-31 02:20:01 -0430

    tutorial

--- js/main.js
+++ js/main.js
@@ -21,8 +21,7 @@ var db
  function onDeviceReady()
  {
 mkLog("Aplicacion cargada");
-            //navigator.notification.alert("PhoneGap is working")
-			
+            
 
 existe_db=window.localStorage.getItem("existe_db");
 db=window.openDatabase("mi_agendita","1.0","Base de dato de mi agenda", 200000);
@@ -32,43 +31,36 @@ if(existe_db == null){
 }
 
 function creaDB(){
+	db.transaction(creaNuevaDB, errorDB, creaSuccess);
 	
-	db.transaction(creaNuevaDB,errorDB,creaSuccess);
-	
-	
-	
-
 }
 function creaNuevaDB(tx){
 	mkLog("Creando base de datos");
 	
 	tx.executeSql("DROP TABLE IF EXISTS mi_agendita");
 	
-	
-	var sql ="CREATE TABLE IF NO EXISTS mi_agendita ( "+
-	"id INTEGER PRIMARY KEY AUTOINCREMET, " +
-	"nombre VARCHAR(50), " +
-	 "apellidos VARCHAR 50), " +
-	 "telefono VARCHAR(50), " +
-	 "categoria VARCHAR(50), " +
-	 "foto VARCHAR(200), " +
-	 "mail VARCHAR(50) ) " ;
+var sql = "CREATE TABLE IF NOT EXISTS agenda_curso ( "+
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+		"nombre VARCHAR(50), " +
+		"apellidos VARCHAR(50), " +
+		"telefono VARCHAR(30), " +
+		"categoria VARCHAR(30), " +
+		"foto VARCHAR(200), " + 
+		"email VARCHAR(30) )";
 	 
 	tx.executeSql(sql);
-	
-		tx.executeSql("INSERT INTO mi_agendita (id, nombre, apellidos, telefono, categoria, foto, mail) VALUES ('1', 'Miguelito', 'Rodriguez', '+584163981767', 'familia','', '', 'pixeladaseg@gmail.com' )");
-			tx.executeSql("INSERT INTO mi_agendita (id, nombre, apellidos, telefono, categoria, foto, mail) VALUES ('2', 'Lenina', 'Rodriguez', '+584248339857', 'familia','', '', 'pixeladaseg@gmail.com' )");
-
+		tx.executeSql("INSERT INTO agenda_curso (id,nombre,apellidos,telefono,categoria,foto,email) VALUES (2,'Siro','González','+34555434567','familia','','siro@test.com')");
+	tx.executeSql("INSERT INTO agenda_curso (id,nombre,apellidos,telefono,categoria,foto,email) VALUES (3,'Julio','Rodríguez','+34756222666','trabajo','','julio@test.com')");
 }
 
-function creaSuccess() {window.localStorage.setItem("existe_db",1);
+function creaSuccess() {
+	window.localStorage.setItem("existe_db",1);
 	
 	}
-	function errorDB(err) {
-		mkLog("error procesando SQL" + err.code);
-		navigator.notification.alert("error procesando SQL" + err.code);
-		
-		}
+function errorDB(err){
+	mkLog("Error procesando SQL " + err.code);
+	navigator.notification.alert("Error procesando SQL " + err.code);
+}
 
 			
 }
